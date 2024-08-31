128b00e6798ae663ae088aff47ad50d8352996bc
ThalysRD <tgbr66@gmail.com>
2024-08-31 08:48:46 -0300

    Estudo de getters and setters na conversão Java Beans

---
+++ pacotes/src/escola/Escola.java
@@ -0,0 +1,13 @@
+package escola;
+
+import escola.Aluno;
+
+public class Escola {
+	public static void main(String[] args) {
+		Aluno felipe = new Aluno();
+		felipe.setNome("Felipe");
+		felipe.setIdade(8);
+		
+		System.out.println("O aluno " + felipe.getNome() + " tem " + felipe.getIdade() + " anos");
+	}
+}
